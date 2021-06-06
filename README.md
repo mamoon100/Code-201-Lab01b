52a8092613e414b966b4836a73dfc6d7037e5489
Mamoun Hussein <mamoon.husen1006@gmail.com>
2021-06-06 15:13:49 +0300

    Added Index.html and js for the first time try1

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Code-201-Lab01b
+
+### This is the solution of Code 201 Lab 01b For the LTUC.
---
+++ app.js
@@ -0,0 +1,8 @@
+let firstQustion = prompt ("Do you like mansaf?")
+let seconeQustion = prompt ("Single or Married?")
+let thirdQustion = prompt ("If you have million dollar what is the car you will buy for evey day use?")
+let fourthQustion = prompt ("How many cups of coffe do you drink a day ?")
+
+alert (`Hello,so you answer ${firstQustion} to eating mansaf which is kind cool and you said that you are ${seconeQustion} and your favourate car is ${thirdQustion} and you drink ${fourthQustion} cups of coffe per day.`)
+
+console.log(`this is for the devoleper side we ask the user four qustions and here is thier answer \n first one answer is: ${firstQustion} \n second one: ${seconeQustion} \n the third one ${thirdQustion} \n and fianally ${fourthQustion} \n if you see 4 answer you have done great job.`)
